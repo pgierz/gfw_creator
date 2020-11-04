36fe2794806c6a9439fc985089d80d800cce4ffc
Paul Gierz <pgierz@awi.de>
2020-11-04 11:37:53 +0100

    feat: works except for unit conversion

--- gfw_creator/cli.py
+++ gfw_creator/cli.py
@@ -1,16 +1,21 @@
 # -*- coding: utf-8 -*-
 
 """Console script for gfw_creator."""
+import gfw_creator
+
 import sys
 import click
 
 
 @click.command()
-def main(args=None):
-    """Console script for gfw_creator."""
-    click.echo("Replace this message by putting your code into "
-               "gfw_creator.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+@click.argument("lat_0", type=float)
+@click.argument("lat_1", type=float)
+@click.argument("lon_0", type=float)
+@click.argument("lon_1", type=float)
+@click.argument("hosing", type=float)
+def main(lat_0, lat_1, lon_0, lon_1, hosing):
+    ds = gfw_creator.create_homogeneous_hosing(lat_0, lat_1, lon_0, lon_1, hosing)
+    ds.to_netcdf("out.nc")
     return 0
 
 
