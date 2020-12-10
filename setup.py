7409a1f27cbd5357b462916265f2634df633cc05
Paul Gierz <pgierz@awi.de>
2020-12-10 09:35:06 +0100

    ci(github-ci): moves to github ci

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=6.0', "xarray", "cdo"]
+requirements = ['Click>=6.0', "xarray", "cdo", "netcdf4",]
 
 setup_requirements = ['pytest-runner', ]
 
