1a33ce1eaa0577518db92dc5ce9ffb03382cb5dd
Paul Gierz <pgierz@awi.de>
2020-12-11 15:42:17 +0100

    plugin works

--- gfw_creator/esm_tools_plugin.py
+++ gfw_creator/esm_tools_plugin.py
@@ -7,7 +7,7 @@ from loguru import logger
 
 def create_hosing_files(config):
     if config["general"]["setup_name"] == "awiesm":
-        if config["fesom"]["do_hosing"]:
+        if config["fesom"].get("do_hosing"):
             logger.info("Creating hosing files...")
             hosing_type = config["fesom"].get("hosing_type")
             if hosing_type == "homogeneous":
@@ -22,11 +22,23 @@ def create_hosing_files(config):
                 gfw_atmo_file.to_netcdf(
                     config["fesom"]["thisrun_forcing_dir"] + "/gfw_atmo.nc"
                 )
-                logger.info(f"Wrote {config['fesom']['thisrun_forcing_dir'] + '/gfw_atmo.nc'} for use in the simulation!")
+                logger.info(
+                    f"Wrote {config['fesom']['thisrun_forcing_dir'] + '/gfw_atmo.nc'} for use in the simulation!"
+                )
                 config["oasis3mct"].setdefault("coupling_input_fields", {})
-                config["oasis3mct"]["coupling_input_fields"]["gfw_atmo"] = {"freq": 999, "field_filepath": "gfw_atmo.nc"}
-                # TODO(PG): Add gfw_atmo.nc to forcing files dict in FESOM...
-                # TODO(PG): Or copy it directly into the work folder from here...
+                config["oasis3mct"]["coupling_input_fields"]["gfw_atmo"] = {
+                    "freq": 86400,
+                    "field_filepath": "gfw_atmo.nc",
+                }
+                config["fesom"].setdefault("forcing_files", {})
+                config["fesom"].setdefault("forcing_sources", {})
+                config["fesom"].setdefault("forcing_in_work", {})
+
+                config["fesom"]["forcing_files"]["gfw_atmo"] = "gfw_atmo"
+                config["fesom"]["forcing_sources"]["gfw_atmo"] = (
+                    config["fesom"]["thisrun_forcing_dir"] + "/gfw_atmo.nc"
+                )
+                config["fesom"]["forcing_in_work"]["gfw_atmo"] = "gfw_atmo.nc"
                 return config
             elif hosing_type == "from_file":
                 logger.error("Sorry, hosing from a file has no yet been implemented!")
